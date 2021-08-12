acf473f37586e837ef74d6062ba0420c76cddaf8
Joe Tsindos <jtsindos@gmail.com>
2021-08-12 12:01:18 +1000

    example 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import styled from 'styled-components'
+
+import './style.css'
+
 const App = () => {
   return (
-    <>
-      Hello
-    </>
+    <div className='container'>
+      <Hug>
+        Hug contents
+      </Hug>
+      <div className='container__box--fillVertical'>
+        Fixed width, fill vertically
+      </div>
+      <div className='container__box--fillHorizontal'>
+        Fixed height, fill horizontally
+      </div>
+      <div className='container__box--fillHorizontalAndVertical'>
+        Fill vertical & horizontal
+      </div>
+    </div>
   )
 }
+
+const Hug = styled.div`
+  padding: 16px;
+  background-color: #FF8577;
+`
 
 const wrapper = document.getElementById('container')
 ReactDOM.render(<App />, wrapper)
